c82c1c613e084879846a59f87a6e6227da392d51
Anthony Wat <anthony_wat@toyota.ca>
2019-04-29 14:23:16 -0400

    Added sapui5 best practice build process

--- VOASGuide/Gruntfile.js
+++ VOASGuide/Gruntfile.js
@@ -1,10 +1,54 @@
 module.exports = function (grunt) {
 	"use strict";
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-	grunt.registerTask("default", ["lint"]);
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
-	grunt.registerTask("unit_and_integration_tests", ["test"]);
 	grunt.config.merge({
+		compatVersion: "1.63",
+		clean: {
+			distRoot: [
+				"dist/*",
+				"!dist/webapp/**"
+			]
+		},
+		copy: {
+			copyProjectFilesToDist: {
+				files: [{
+					src: "!*-app.json"
+				}]
+			},
+			distRootToDistWebapp: {
+				cwd: "dist",
+				expand: true,
+				src: [
+					"**",
+					"!webapp/**"
+				],
+				dest: "dist/webapp"
+			},
+			rootToDistRoot: {
+				cwd: ".",
+				expand: true,
+				src: [
+					"**",
+					"!dist/**",
+					"!webapp/**"
+				],
+				dest: "dist",
+			}
+		},
+		mkdir: {
+			distWebapp: {
+				options: {
+					create: ["dist/webapp"]
+				}
+			}
+		},
+		shell: {
+			npmPrune: {
+				cwd: "dist",
+				command: "npm prune --production"
+			}
+		},
 		coverage_threshold: {
 			statements: 0,
 			branches: 100,
@@ -12,4 +56,21 @@ module.exports = function (grunt) {
 			lines: 0
 		}
 	});
+	grunt.loadNpmTasks("grunt-shell");
+	grunt.registerTask("updateDistToNodeJsStructure", [
+		"mkdir:distWebapp",
+		"copy:distRootToDistWebapp",
+		"clean:distRoot",
+		"copy:rootToDistRoot",
+		"shell:npmPrune"
+	]);
+	grunt.registerTask("default", [
+		"clean",
+		"lint",
+		"build",
+		"updateDistToNodeJsStructure"
+	]);
+	grunt.registerTask("unit_and_integration_tests", [
+		"test"
+	]);
 };
