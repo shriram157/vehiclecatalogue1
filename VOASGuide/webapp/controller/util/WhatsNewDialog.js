b3af3b97c3abe4cb15e8f09f21333f886fc173ca
Harlalka Ravi <ravi_harlalka@toyota.ca>
2019-05-10 05:59:22 +0000

    upload file - 	reader.readAsArrayBuffer(file);

--- VOASGuide/webapp/controller/util/WhatsNewDialog.js
+++ VOASGuide/webapp/controller/util/WhatsNewDialog.js
@@ -93,7 +93,132 @@ sap.ui.define([
 						}
 					});
 			},*/
-		_uploadPDFFile: function (oEvent) {
+		// _uploadPDFFile: function (oEvent) {
+		// 	var modelAdm = sap.ui.getCore().getModel("modelAdmin");
+		// 	var modelAdmData = modelAdm.getData();
+		// 	oEvent = jQuery.extend(true, {}, oEvent);
+		// 	return new Promise(function (fnResolve) {
+		// 			fnResolve(true);
+		// 		})
+		// 		.then(function (result) {
+		// 			var sLocation = window.location.host;
+		// 			var sLocation_conf = sLocation.search("webide");
+		// 			if (sLocation_conf == 0) {
+		// 				this.sPrefix = "/voasguide_node";
+		// 			} else {
+		// 				this.sPrefix = "";
+		// 			}
+		// 			this.nodeJsUrl = this.sPrefix + "/node";
+		// 			var comment = this._oView.byId("__component0---AdminDetailsOption--whatsNew_TA_Comment").getValue(); // "ok"; 
+		// 			var filename = this._oView.byId("__component0---AdminDetailsOption--whatsNewFileUploadId").mProperties.value; //"abc.pdf";
+		// 			var date = new Date();
+		// 			var date2 = sap.ui.core.format.DateFormat.getDateInstance({
+		// 				pattern: "yyyyMMdd"
+		// 			});
+		// 			var date3 = date2.format(date);
+		// 			var langSwitchState = this._oView.byId("__component0---AdminDetailsOption--whatsNew_Switch_Lang").mProperties.state;
+		// 			var lang = "";
+		// 			if (langSwitchState == false) {
+		// 				lang = "FR";
+		// 			} else {
+		// 				lang = "EN";
+		// 			}
+
+		// 			//var oURL = this.nodeJsUrl + "/Z_VEHICLE_CATALOGUE_SRV/FileSet('" + filename + "')/$value";
+		// 			var oFileUploader = this._oView.byId("__component0---AdminDetailsOption--whatsNewFileUploadId");
+		// 			var oURL4 = this.nodeJsUrl + "/Z_VEHICLE_CATALOGUE_SRV/FileSet(Comment='" + comment + "',FileName='" + filename + "',Language='" +
+		// 				lang + "',Lastupdate='" + date3 + "',Tab='WhatsNew')/$value";
+		// 			var oURL2 = this.nodeJsUrl + "/Z_VEHICLE_CATALOGUE_SRV/FileSet(Comment='" + comment + "',FileName='" + filename + "',Language='" +
+		// 				lang + "',Lastupdate='" + date3 + "',Tab='WhatsNew',Model='" + modelAdmData.modelDesc + "',Model_year='" + modelAdmData.moYear +
+		// 				"',Tciseries='" + modelAdmData.series + "',Brand='" + modelAdmData.brand + "')/$value";
+		// 			var oUrl5 = this.nodeJsUrl + "/Z_VEHICLE_CATALOGUE_SRV/FileReadSet?$filter=(Tab eq 'WhatsNew')";
+		// 			var oUrl3 = this.nodeJsUrl + "/Z_VEHICLE_CATALOGUE_SRV/FileReadSet?$filter=(Tab eq 'WhatsNew' and Model eq '" + modelAdmData.modelDesc +
+		// 				"' and Model_year eq '" + modelAdmData.moYear + "' and Tciseries eq '" + modelAdmData.series + "' and Brand eq '" +
+		// 				modelAdmData.brand + "')";
+		// 			var token;
+		// 			var oBusyDialog = new sap.m.BusyDialog({
+		// 		showCancelButton: false
+		// 	});
+
+		// 			var tbl = sap.ushell.components.whatsNewTbl;
+		// 			var file = jQuery.sap.domById(oFileUploader.getId() + "-fu").files[0];
+		// 			var base64_marker = 'data:' + file.type + ';base64,';
+		// 			var reader = new FileReader();
+		// 			reader.onload = function readSuccess(evt) {
+		// 				var base64Index = evt.target.result.indexOf(base64_marker) + base64_marker.length;
+		// 				var _base64 = evt.target.result.substring(base64Index);
+		// 				$.ajax({
+		// 					url: oURL2,
+		// 					type: 'GET',
+		// 					beforeSend: function (xhr) {
+		// 						xhr.setRequestHeader("X-CSRF-Token", "Fetch");
+		// 					},
+		// 					complete: function (xhr) {
+		// 						token = xhr.getResponseHeader("X-CSRF-Token");
+		// 					oBusyDialog.open(); 
+		// 						$.ajax({
+		// 							type: 'PUT',
+		// 							url: oURL2,
+		// 							data: _base64,
+		// 							dataType: 'json',
+		// 							beforeSend: function (xhr) {
+		// 								xhr.setRequestHeader('X-CSRF-Token', token);
+		// 								xhr.setRequestHeader('Content-Type',"application/pdf");
+										
+		// 							},
+		// 							success: function (data) {
+		// 							 oBusyDialog.close(); 
+		// 								console.log("PUT success: " + data);
+		// 								$.ajax({
+		// 									url: oUrl3,
+		// 									method: 'GET',
+		// 									async: false,
+		// 									dataType: 'json',
+		// 									success: function (data, textStatus, jqXHR) {
+		// 										console.log("GET success: ");
+		// 										console.log(data.d.results);
+		// 										var tblModel = new sap.ui.model.json.JSONModel(data.d.results);
+		// 										sap.ui.getCore().setModel(tblModel, "whatsNewTblModel");
+		// 										tbl.setModel(tblModel, "whatsNewTblModel");
+		// 									},
+		// 									error: function (jqXHR, textStatus, errorThrown) {
+
+		// 										sap.m.MessageBox.show("Error occurred while fetching data. Please try again later.", sap.m.MessageBox.Icon.ERROR,
+		// 											"Error",
+		// 											sap
+		// 											.m.MessageBox.Action.OK, null, null);
+		// 									}
+		// 								});
+		// 							},
+		// 							error: function (data) {
+		// 															oBusyDialog.close(); 
+
+		// 								sap.m.MessageBox.show("Error occurred while sending data. Please try again later.", sap.m.MessageBox.Icon.ERROR,
+		// 									"Error", sap
+		// 									.m.MessageBox.Action.OK, null, null);
+		// 							}
+
+		// 						});
+		// 					}
+		// 				});
+		// 			};
+		// 			reader.readAsDataURL(file);
+
+		// 		}.bind(this))
+		// 		.then(function (result) {
+		// 			if (result === false) {
+		// 				return false;
+		// 			} else {
+		// 				this.close();
+		// 			}
+		// 		}.bind(this)).catch(function (err) {
+		// 			if (err !== undefined) {
+		// 				MessageBox.error(err.message);
+		// 			}
+		// 		});
+		// },
+		
+			_uploadPDFFile: function (oEvent) {
 			var modelAdm = sap.ui.getCore().getModel("modelAdmin");
 			var modelAdmData = modelAdm.getData();
 			oEvent = jQuery.extend(true, {}, oEvent);
@@ -144,10 +269,7 @@ sap.ui.define([
 					var file = jQuery.sap.domById(oFileUploader.getId() + "-fu").files[0];
 					var base64_marker = 'data:' + file.type + ';base64,';
 					var reader = new FileReader();
-					reader.onload = function readSuccess(evt) {
-						var base64Index = evt.target.result.indexOf(base64_marker) + base64_marker.length;
-						var _base64 = evt.target.result.substring(base64Index);
-						$.ajax({
+					$.ajax({
 							url: oURL2,
 							type: 'GET',
 							beforeSend: function (xhr) {
@@ -155,15 +277,27 @@ sap.ui.define([
 							},
 							complete: function (xhr) {
 								token = xhr.getResponseHeader("X-CSRF-Token");
-							oBusyDialog.open(); 
+							} 
+						});
+					//var fileString = evt.target.result
+					reader.onload = function readSuccess(evt) {
+						var fileString = evt.target.result;
+						//var base64Index = evt.target.result.indexOf(base64_marker) + base64_marker.length;
+						//var _base64 = evt.target.result.substring(base64Index);
+						 var filename = file.name;
+                         var  extension = file.type;
+                         var slug = filename;
+						
+							//oBusyDialog.open(); 
 								$.ajax({
 									type: 'PUT',
 									url: oURL2,
-									data: _base64,
+									data: file,
 									dataType: 'json',
 									beforeSend: function (xhr) {
 										xhr.setRequestHeader('X-CSRF-Token', token);
 										xhr.setRequestHeader('Content-Type',"application/pdf");
+										xhr.setRequestHeader('slug', file.name);
 										
 									},
 									success: function (data) {
@@ -199,10 +333,11 @@ sap.ui.define([
 									}
 
 								});
-							}
-						});
-					};
-					reader.readAsDataURL(file);
+							};
+						
+					//};
+					reader.readAsArrayBuffer(file);
+					//reader.readAsDataURL(file);
 
 				}.bind(this))
 				.then(function (result) {
