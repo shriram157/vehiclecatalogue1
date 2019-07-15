3d78796dfa75100f070a76ee597b73a377fc3426
Anthony Wat <anthony_wat@toyota.ca>
2019-07-15 15:11:18 -0400

    Added uncaught error handling to Node APIs

--- voasGuide_Node/router/routes/api-proxy.js
+++ voasGuide_Node/router/routes/api-proxy.js
@@ -34,74 +34,78 @@ module.exports = function (appContext) {
 	router.all("/*", function (req, res, next) {
 		var logger = req.loggingContext.getLogger("/Application/Route/APIProxy");
 		var tracer = req.loggingContext.getTracer(__filename);
-		var proxiedMethod = req.method;
-		var proxiedReqHeaders = {
-			"APIKey": APIKey,
-			"Content-Type": req.get("Content-Type")
-		};
-		var proxiedUrl = apimUrl + req.url;
-
-		// Add/update sap-client query parameter with UPS value in the proxied URL
-		var proxiedUrlObj = new URL(proxiedUrl);
-		proxiedUrlObj.searchParams.delete("sap-client");
-		proxiedUrlObj.searchParams.set("sap-client", s4Client);
-		proxiedUrl = proxiedUrlObj.href;
-
-		proxiedReqHeaders.Authorization = "Basic " + new Buffer(s4User + ":" + s4Password).toString("base64");
-
-		// Pass through x-csrf-token from request to proxied request to S4/HANA
-		// This requires manual handling of CSRF tokens from the front-end
-		// Note: req.get() will get header in a case-insensitive manner 
-		var csrfTokenHeaderValue = req.get("X-Csrf-Token");
-		proxiedReqHeaders["X-Csrf-Token"] = csrfTokenHeaderValue;
-
-		// Redact security-sensitive header values before writing to trace log
-		var traceProxiedReqHeaders = JSON.parse(JSON.stringify(proxiedReqHeaders));
-		var secSensitiveHeaderNames = ["authorization", "apikey", "x-csrf-token"];
-		Object.keys(traceProxiedReqHeaders).forEach(key => {
-			if (secSensitiveHeaderNames.includes(key.toLowerCase())) {
-				traceProxiedReqHeaders[key] = "REDACTED";
-			}
-		});
-
-		tracer.debug("Proxied Method: %s", proxiedMethod);
-		tracer.debug("Proxied request headers: %s", JSON.stringify(traceProxiedReqHeaders));
-		tracer.debug("Proxied URL: %s", proxiedUrl);
-
-		let proxiedReq = request({
-			headers: proxiedReqHeaders,
-			method: proxiedMethod,
-			url: proxiedUrl
-		});
-
-		// Remove MYSAPSSO2 cookie before making the proxied request, so that it does not override basic auth when APIM
-		// proxies the request to SAP Gateway
-		if ("cookie" in req.headers) {
-			tracer.debug("Original cookies: %s", req.headers.cookie);
-			var cookies = req.headers.cookie.split(";");
-			var filteredCookies = "";
-			cookies.forEach(cookie => {
-				var sepIndex = cookie.indexOf("=");
-				var cookieName = cookie.substring(0, sepIndex).trim();
-				var cookieValue = cookie.substring(sepIndex + 1).trim();
-				if (cookieName !== "MYSAPSSO2") {
-					filteredCookies += (filteredCookies.length > 0 ? "; " : "") + cookieName + "=" + cookieValue;
+		try {
+			var proxiedMethod = req.method;
+			var proxiedReqHeaders = {
+				"APIKey": APIKey,
+				"Content-Type": req.get("Content-Type")
+			};
+			var proxiedUrl = apimUrl + req.url;
+	
+			// Add/update sap-client query parameter with UPS value in the proxied URL
+			var proxiedUrlObj = new URL(proxiedUrl);
+			proxiedUrlObj.searchParams.delete("sap-client");
+			proxiedUrlObj.searchParams.set("sap-client", s4Client);
+			proxiedUrl = proxiedUrlObj.href;
+	
+			proxiedReqHeaders.Authorization = "Basic " + new Buffer(s4User + ":" + s4Password).toString("base64");
+	
+			// Pass through x-csrf-token from request to proxied request to S4/HANA
+			// This requires manual handling of CSRF tokens from the front-end
+			// Note: req.get() will get header in a case-insensitive manner 
+			var csrfTokenHeaderValue = req.get("X-Csrf-Token");
+			proxiedReqHeaders["X-Csrf-Token"] = csrfTokenHeaderValue;
+	
+			// Redact security-sensitive header values before writing to trace log
+			var traceProxiedReqHeaders = JSON.parse(JSON.stringify(proxiedReqHeaders));
+			var secSensitiveHeaderNames = ["authorization", "apikey", "x-csrf-token"];
+			Object.keys(traceProxiedReqHeaders).forEach(key => {
+				if (secSensitiveHeaderNames.includes(key.toLowerCase())) {
+					traceProxiedReqHeaders[key] = "REDACTED";
 				}
 			});
-			tracer.debug("Filtered cookies: %s", filteredCookies);
-			req.headers.cookie = filteredCookies;
+	
+			tracer.debug("Proxied Method: %s", proxiedMethod);
+			tracer.debug("Proxied request headers: %s", JSON.stringify(traceProxiedReqHeaders));
+			tracer.debug("Proxied URL: %s", proxiedUrl);
+	
+			let proxiedReq = request({
+				headers: proxiedReqHeaders,
+				method: proxiedMethod,
+				url: proxiedUrl
+			});
+	
+			// Remove MYSAPSSO2 cookie before making the proxied request, so that it does not override basic auth when APIM
+			// proxies the request to SAP Gateway
+			if ("cookie" in req.headers) {
+				tracer.debug("Original cookies: %s", req.headers.cookie);
+				var cookies = req.headers.cookie.split(";");
+				var filteredCookies = "";
+				cookies.forEach(cookie => {
+					var sepIndex = cookie.indexOf("=");
+					var cookieName = cookie.substring(0, sepIndex).trim();
+					var cookieValue = cookie.substring(sepIndex + 1).trim();
+					if (cookieName !== "MYSAPSSO2") {
+						filteredCookies += (filteredCookies.length > 0 ? "; " : "") + cookieName + "=" + cookieValue;
+					}
+				});
+				tracer.debug("Filtered cookies: %s", filteredCookies);
+				req.headers.cookie = filteredCookies;
+			}
+	
+			req.pipe(proxiedReq);
+			proxiedReq.on("response", proxiedRes => {
+				tracer.debug("Proxied call %s %s successful.", proxiedMethod, proxiedUrl);
+				delete proxiedRes.headers.cookie;
+	
+				proxiedReq.pipe(res);
+			}).on("error", error => {
+				logger.error("Proxied call %s %s FAILED: %s", proxiedMethod, proxiedUrl, error);
+				next(error);
+			});
+		} catch (err) {
+			next(err);
 		}
-
-		req.pipe(proxiedReq);
-		proxiedReq.on("response", proxiedRes => {
-			tracer.debug("Proxied call %s %s successful.", proxiedMethod, proxiedUrl);
-			delete proxiedRes.headers.cookie;
-
-			proxiedReq.pipe(res);
-		}).on("error", error => {
-			logger.error("Proxied call %s %s FAILED: %s", proxiedMethod, proxiedUrl, error);
-			next(error);
-		});
 	});
 
 	return router;
--- voasGuide_Node/router/routes/user-details.js
+++ voasGuide_Node/router/routes/user-details.js
@@ -21,95 +21,103 @@ module.exports = function (appContext) {
 		}
 	}).xsuaa;
 
-	router.get("/attributes", (req, res) => {
+	router.get("/attributes", (req, res, next) => {
 		var logger = req.loggingContext.getLogger("/Application/Route/UserDetails/Attributes");
 		var tracer = req.loggingContext.getTracer(__filename);
-		var userProfile = req.user;
-		var userAttributes = req.authInfo.userAttributes;
-		tracer.debug("User profile from JWT: %s", JSON.stringify(userProfile));
-		tracer.debug("User attributes from JWT: %s", JSON.stringify(userAttributes));
-
-		// If there is no user type, it is most probably a call from Neo, in which case we can fake the data as TCI user
-		if (mockUserMode && mockUserOrigin === req.authInfo.origin) {
-			userAttributes = {
-				Language: ["English"],
-				UserType: ["National"]
+		try {
+			var userProfile = req.user;
+			var userAttributes = req.authInfo.userAttributes;
+			tracer.debug("User profile from JWT: %s", JSON.stringify(userProfile));
+			tracer.debug("User attributes from JWT: %s", JSON.stringify(userAttributes));
+	
+			// If there is no user type, it is most probably a call from Neo, in which case we can fake the data as TCI user
+			if (mockUserMode && mockUserOrigin === req.authInfo.origin) {
+				userAttributes = {
+					Language: ["English"],
+					UserType: ["National"]
+				};
+				tracer.debug("Mock user mode is enabled and JWT is from origin %s, switch to mock user attributes: %s", req.authInfo.origin, JSON.stringify(
+					userAttributes));
+			}
+	
+			var resBody = {
+				"userProfile": userProfile,
+				"samlAttributes": userAttributes
 			};
-			tracer.debug("Mock user mode is enabled and JWT is from origin %s, switch to mock user attributes: %s", req.authInfo.origin, JSON.stringify(
-				userAttributes));
+	
+			tracer.debug("Response body: %s", JSON.stringify(resBody));
+			return res.type("application/json").status(200).send(resBody);
+		} catch (err) {
+			next(err);
 		}
-
-		var resBody = {
-			"userProfile": userProfile,
-			"samlAttributes": userAttributes
-		};
-
-		tracer.debug("Response body: %s", JSON.stringify(resBody));
-		return res.type("application/json").status(200).send(resBody);
 	});
 
-	router.get("/currentScopesForUser", (req, res) => {
+	router.get("/currentScopesForUser", (req, res, next) => {
 		var logger = req.loggingContext.getLogger("/Application/Route/UserDetails/CurrentScopesForUser");
 		var tracer = req.loggingContext.getTracer(__filename);
-		var xsAppName = xsuaaService.xsappname;
-		var scopes = req.authInfo.scopes;
-		var userAttributes = req.authInfo.userAttributes;
-
-		tracer.debug("Scopes from JWT: %s", JSON.stringify(scopes));
-		tracer.debug("User attributes from JWT: %s", JSON.stringify(userAttributes));
-
-		// If there is no user type, it is most probably a call from Neo, in which case we can fake the data as TCI user
-		if (mockUserMode && mockUserOrigin === req.authInfo.origin) {
-			tracer.debug("Mock user mode is enabled and JWT is from origin %s, switch to mock user type.", req.authInfo.origin);
-			return res.type("application/json").status(200).send(JSON.stringify({
-				loggedUserType: ["TCI_User"]
-			}));
-		}
-
-		var role = "Unknown";
-		var manageVOASGuide = false;
-		var viewVOASGuidesDealerNet = false;
-		var viewVOASGuidesMSRP = false;
-		var viewVOASGuidesUnreleased = false;
-
-		for (var i = 0; i < scopes.length; i++) {
-			if (scopes[i] === xsAppName + ".Manage_VOAS_Guides") {
-				manageVOASGuide = true;
-			} else if (scopes[i] === xsAppName + ".View_VOAS_Guides_Dealer_Net") {
-				viewVOASGuidesDealerNet = true;
-			} else if (scopes[i] === xsAppName + ".View_VOAS_Guides_MSRP") {
-				viewVOASGuidesMSRP = true;
-			} else if (scopes[i] === xsAppName + ".View_VOAS_Guides_Unreleased") {
-				viewVOASGuidesUnreleased = true;
-			} else {
-				tracer.warning("Unrecognized scope: %s", scopes[i]);
+		try {
+			var xsAppName = xsuaaService.xsappname;
+			var scopes = req.authInfo.scopes;
+			var userAttributes = req.authInfo.userAttributes;
+	
+			tracer.debug("Scopes from JWT: %s", JSON.stringify(scopes));
+			tracer.debug("User attributes from JWT: %s", JSON.stringify(userAttributes));
+	
+			// If there is no user type, it is most probably a call from Neo, in which case we can fake the data as TCI user
+			if (mockUserMode && mockUserOrigin === req.authInfo.origin) {
+				tracer.debug("Mock user mode is enabled and JWT is from origin %s, switch to mock user type.", req.authInfo.origin);
+				return res.type("application/json").status(200).send(JSON.stringify({
+					loggedUserType: ["TCI_User"]
+				}));
 			}
+	
+			var role = "Unknown";
+			var manageVOASGuide = false;
+			var viewVOASGuidesDealerNet = false;
+			var viewVOASGuidesMSRP = false;
+			var viewVOASGuidesUnreleased = false;
+	
+			for (var i = 0; i < scopes.length; i++) {
+				if (scopes[i] === xsAppName + ".Manage_VOAS_Guides") {
+					manageVOASGuide = true;
+				} else if (scopes[i] === xsAppName + ".View_VOAS_Guides_Dealer_Net") {
+					viewVOASGuidesDealerNet = true;
+				} else if (scopes[i] === xsAppName + ".View_VOAS_Guides_MSRP") {
+					viewVOASGuidesMSRP = true;
+				} else if (scopes[i] === xsAppName + ".View_VOAS_Guides_Unreleased") {
+					viewVOASGuidesUnreleased = true;
+				} else {
+					tracer.warning("Unrecognized scope: %s", scopes[i]);
+				}
+			}
+	
+			var scopeLogMessage = "manageVOASGuide: " + manageVOASGuide + "\n";
+			scopeLogMessage += "viewVOASGuidesDealerNet: " + viewVOASGuidesDealerNet + "\n";
+			scopeLogMessage += "viewVOASGuidesMSRP: " + viewVOASGuidesMSRP + "\n";
+			scopeLogMessage += "viewVOASGuidesUnreleased: " + viewVOASGuidesUnreleased + "\n";
+			tracer.debug(scopeLogMessage);
+	
+			if (!manageVOASGuide && viewVOASGuidesDealerNet && viewVOASGuidesMSRP && !viewVOASGuidesUnreleased) {
+				role = (userAttributes.UserType[0] === "Dealer") ? "Dealer_Admin" : "TCI_User_Dealer_Net";
+			} else if (!manageVOASGuide && !viewVOASGuidesDealerNet && viewVOASGuidesMSRP && !viewVOASGuidesUnreleased) {
+				role = (userAttributes.UserType[0] === "Dealer") ? "Dealer_User" : "TCI_User";
+			} else if (manageVOASGuide && viewVOASGuidesDealerNet && viewVOASGuidesMSRP && viewVOASGuidesUnreleased) {
+				role = "TCI_Admin";
+			} else if (!manageVOASGuide && !viewVOASGuidesDealerNet && viewVOASGuidesMSRP && viewVOASGuidesUnreleased) {
+				role = "TCI_User_Preliminary";
+			} else if (!manageVOASGuide && viewVOASGuidesDealerNet && viewVOASGuidesMSRP && viewVOASGuidesUnreleased) {
+				role = "TCI_User_Preliminary_Dealer_Net";
+			}
+			tracer.debug("role: %s", role);
+	
+			return res.type("application/json").status(200).send(JSON.stringify({
+				loggedUserType: [
+					role
+				]
+			}));
+		} catch (err) {
+			next(err);
 		}
-
-		var scopeLogMessage = "manageVOASGuide: " + manageVOASGuide + "\n";
-		scopeLogMessage += "viewVOASGuidesDealerNet: " + viewVOASGuidesDealerNet + "\n";
-		scopeLogMessage += "viewVOASGuidesMSRP: " + viewVOASGuidesMSRP + "\n";
-		scopeLogMessage += "viewVOASGuidesUnreleased: " + viewVOASGuidesUnreleased + "\n";
-		tracer.debug(scopeLogMessage);
-
-		if (!manageVOASGuide && viewVOASGuidesDealerNet && viewVOASGuidesMSRP && !viewVOASGuidesUnreleased) {
-			role = (userAttributes.UserType[0] === "Dealer") ? "Dealer_Admin" : "TCI_User_Dealer_Net";
-		} else if (!manageVOASGuide && !viewVOASGuidesDealerNet && viewVOASGuidesMSRP && !viewVOASGuidesUnreleased) {
-			role = (userAttributes.UserType[0] === "Dealer") ? "Dealer_User" : "TCI_User";
-		} else if (manageVOASGuide && viewVOASGuidesDealerNet && viewVOASGuidesMSRP && viewVOASGuidesUnreleased) {
-			role = "TCI_Admin";
-		} else if (!manageVOASGuide && !viewVOASGuidesDealerNet && viewVOASGuidesMSRP && viewVOASGuidesUnreleased) {
-			role = "TCI_User_Preliminary";
-		} else if (!manageVOASGuide && viewVOASGuidesDealerNet && viewVOASGuidesMSRP && viewVOASGuidesUnreleased) {
-			role = "TCI_User_Preliminary_Dealer_Net";
-		}
-		tracer.debug("role: %s", role);
-
-		return res.type("application/json").status(200).send(JSON.stringify({
-			loggedUserType: [
-				role
-			]
-		}));
 	});
 
 	return router;
